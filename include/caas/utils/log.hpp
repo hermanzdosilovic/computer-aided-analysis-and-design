75f1c8a7193ec748f380e647f86c63308f1bd3f3
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2018-11-05 18:25:35 +0100

    Fixed typo in log.hpp

--- include/caas/utils/log.hpp
+++ include/caas/utils/log.hpp
@@ -14,13 +14,13 @@
 #define COLOR_DEFAULT ""
 #endif
 
-#define N0OP ( ( void ) 0 )
+#define NOOP ( ( void ) 0 )
 #define DETAILS( file ) std::fprintf( file, "[%s %s] %s:%d\n%s", __DATE__, __TIME__, __FILE__, __LINE__, __PRETTY_FUNCTION__ );
 
 #ifndef NTRACE
 #define TRACE DETAILS std::printf( "\n" );
 #else
-#define TRACE N0OP;
+#define TRACE NOOP;
 #endif
 
 #define LOG_ERROR( fmt, ... ) DETAILS( stderr ) std::fprintf( stderr, "\n" COLOR_RED fmt COLOR_DEFAULT "\n", ##__VA_ARGS__ )
@@ -28,5 +28,5 @@
 #ifndef NDEBUG
 #define LOG_INFO( fmt, ... ) DETAILS( stdout ) std::printf( "\n" COLOR_YELLOW fmt COLOR_DEFAULT "\n", ##__VA_ARGS__ )
 #else
-#define LOG_INFO( fmt, ... ) NO0P
+#define LOG_INFO( fmt, ... ) NOOP
 #endif
